6a22a066b1580e50f4eb9000958749ce20e1ca4e
Max Horwich <32074435+mhorwich@users.noreply.github.com>
2020-08-13 01:54:02 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,99 @@
+var modalStampin = document.getElementById("modalStampin");
+var stampin = document.getElementById("stampin-button");
+stampin.onclick = function() {
+  modalStampin.style.display = "block";
+};
+var spanStampin = document.getElementsByClassName("closeStampin")[0];
+spanStampin.onclick = function() {
+  modalStampin.style.display = "none";
+};
+
+var modalDoug = document.getElementById("modalDoug");
+var doug = document.getElementById("doug");
+doug.onclick = function() {
+  modalDoug.style.display = "block";
+};
+var spanDoug = document.getElementsByClassName("closeDoug")[0];
+spanDoug.onclick = function() {
+  modalDoug.style.display = "none";
+};
+
+var modalMaryanne = document.getElementById("modalMaryanne");
+var maryanne = document.getElementById("maryanne");
+maryanne.onclick = function() {
+  modalMaryanne.style.display = "block";
+};
+var spanMaryanne = document.getElementsByClassName("closeMaryanne")[0];
+spanMaryanne.onclick = function() {
+  modalMaryanne.style.display = "none";
+};
+
+var modalMarcel = document.getElementById("modalMarcel");
+var marcel = document.getElementById("marcel");
+marcel.onclick = function() {
+  modalMarcel.style.display = "block";
+};
+var spanMarcel = document.getElementsByClassName("closeMarcel")[0];
+spanMarcel.onclick = function() {
+  modalMarcel.style.display = "none";
+};
+
+var modalJess = document.getElementById("modalJess");
+var jess = document.getElementById("jess");
+jess.onclick = function() {
+  modalJess.style.display = "block";
+};
+var spanJess = document.getElementsByClassName("closeJess")[0];
+spanJess.onclick = function() {
+  modalJess.style.display = "none";
+};
+
+var modalPippa = document.getElementById("modalPippa");
+var pippa = document.getElementById("pippa");
+pippa.onclick = function() {
+  modalPippa.style.display = "block";
+};
+var spanPippa = document.getElementsByClassName("closePippa")[0];
+spanPippa.onclick = function() {
+  modalPippa.style.display = "none";
+};
+
+var modalYiting = document.getElementById("modalYiting");
+var yiting = document.getElementById("yiting");
+yiting.onclick = function() {
+  modalYiting.style.display = "block";
+};
+var spanYiting = document.getElementsByClassName("closeYiting")[0];
+spanYiting.onclick = function() {
+  modalYiting.style.display = "none";
+};
+
+var modalMax = document.getElementById("modalMax");
+var max = document.getElementById("max");
+max.onclick = function() {
+  modalMax.style.display = "block";
+};
+var spanMax = document.getElementsByClassName("closeMax")[0];
+spanMax.onclick = function() {
+  modalMax.style.display = "none";
+};
+
+var modalSignup = document.getElementById("modalSignup");
+var signup = document.getElementById("mailing-list");
+signup.onclick = function() {
+  modalSignup.style.display = "block";
+};
+var spanSignup = document.getElementsByClassName("closeSignup")[0];
+spanSignup.onclick = function() {
+  modalSignup.style.display = "none";
+};
+
+var modalContact = document.getElementById("modalContact");
+var contact = document.getElementById("contact");
+contact.onclick = function() {
+  modalContact.style.display = "block";
+};
+var spanContact = document.getElementsByClassName("closeContact")[0];
+spanContact.onclick = function() {
+  modalContact.style.display = "none";
+};
